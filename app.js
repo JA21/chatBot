f57b200d9554352664db4172d3b3f7c0e606d8b8
JA21 <jorgeandres2104@gmail.com>
2022-03-07 09:01:17 -0500

    feat:(send image for whatsapp)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const venom = require('venom-bot');
 const dialogflow = require('./dialogflow');
 const fs = require('fs');
 const mime = require('mime-types');
+const { error } = require('console');
 
 const sessionIds = new Map();
 venom
@@ -21,12 +22,19 @@ function start(client) {
     let session = sessionIds.get(message.from);
     let payload = await dialogflow.sendToDialogFlow(message.body, session)
     let responses = payload.fulfillmentMessages;
+
     for (const response of responses) {
-      sendMessageToWhatsapp(client, message, response);
+      await sendMessageToWhatsapp(client, message, response);
 
     }
 
-    if (message.isMedia === true || message.isMMS === true) {
+    console.log("================================")
+    console.log(message.sender.id, "message message")
+    await sendImageToWhatsapp(client, message);
+
+
+
+    /* if (message.isMedia === true || message.isMMS === true) {
       const buffer = await client.decryptFile(message);
       // At this point you can do whatever you want with the buffer
       // Most likely you want to write it into a file
@@ -38,13 +46,15 @@ function start(client) {
         console.log("File write succes,", fs.readFileSync(fileName))
       }
       });
-    }
+    } */
 
   });
 }
 
-function sendMessageToWhatsapp(client, message, response) {
-  return new Promise((resolve, reject) => {
+async function sendMessageToWhatsapp(client, message, response) {
+  //await startWriting(client, message.sender.id);
+  let promise= new Promise((resolve, reject) => {
+    
     client
       .sendText(message.from, response.text.text[0])
       .then((result) => {
@@ -55,7 +65,11 @@ function sendMessageToWhatsapp(client, message, response) {
         console.error('Error when sending: ', erro);
         reject(erro)
       });
+
   })
+ // await  stoptWriting(client,message.sender.id);
+
+  return promise;
 }
 
 async function setSessionAndUser(senderId) {
@@ -65,3 +79,67 @@ async function setSessionAndUser(senderId) {
     throw error;
   }
 }
+
+
+async function sendImageToWhatsapp(client, message) {
+  console.log("enviando imagen")
+  return new Promise((resolve, reject) => {
+    if (message.text == "image") {
+      console.log("enviando image")
+      client
+        .sendImage(
+          message.sender.id,
+          'image/ubicacion.png',
+          'ubicacion',
+          'Imagen'
+        )
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+          resolve(result);
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+          reject(erro)
+        });
+    }
+  }
+
+  ).catch(error => console.log(error))
+}
+
+async function startWriting(client, to) {
+
+  return new Promise((resolve, reject) => {
+
+    client
+      .startTyping(to)
+      .then((result) => {
+        console.log('Result: ', result); //return object success
+        resolve(result);
+      })
+      .catch((erro) => {
+        console.error('Error when sending: ', erro); //return object error
+        reject(erro)
+      });
+  }
+  ).catch(error => console.log(error, "error in the promises"));
+}
+
+async function stoptWriting(client, to) {
+
+  return new Promise((resolve, reject) => {
+
+    client
+      stopTyping(to)
+      .then((result) => {
+        console.log('Result: ', result); //return object success
+        resolve(result);
+      })
+      .catch((erro) => {
+        console.error('Error when sending: ', erro); //return object error
+        reject(erro)
+      });
+  }
+  ).catch(error => console.log(error, "error in the promises"));
+}
+
